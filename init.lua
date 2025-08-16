531f6a912dd89f4bb11468592a4303cf2d48c35e
Shiven Kashyap <shivenkashyap24@gmail.com>
2025-08-17 01:35:10 +0530

    - Add Treesitter config - Nvim api for setting some opts - Lots of cleanup

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
-vim.o.mouse	 			= 'a'
-vim.o.autoindent 		= true
-vim.o.expandtab			= false			-- set noexpandtab
--- yeah. tabs.
-vim.o.shiftwidth		= 4
-vim.o.tabstop			= 4
+vim.o.mouse	= 'a'
+vim.o.autoindent = true
+vim.o.expandtab = true
 vim.o.relativenumber 	= true
 vim.o.number 			= true
 vim.o.smartindent 		= true
@@ -14,13 +11,31 @@ vim.o.smartcase			= true
 vim.o.wrap				= false
 vim.o.cursorline		= false
 vim.o.hlsearch			= true
+vim.api.nvim_set_option("clipboard","unnamedplus") 
 
--- needs the GVim package, which is compiled with X support,
--- for clipboard functionality
-vim.o.clipboard 		= 'unnamedplus'
+-- tabs are a disaster
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.g.indent = 2
+-- apply indent to buffer
+vim.api.nvim_create_autocmd({ "BufEnter" }, { pattern = "*.*" , command = "let &shiftwidth=g:indent | let &tabstop=g:indent | set expandtab" })
 
--- for some reason, files with the .v extension weren't being recognized
--- as Verilog/SystemVerilog sources.
 vim.cmd("au BufNewFile,BufFilePre,BufRead *.v setfiletype verilog")
+
+-- treesitter stuff
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+  sync_install = false,
+  auto_install = false,
+  ignore_install = { "javascript" },
+
+  highlight = {
+    enable = true,
+    disable = { "c", "rust", "python" },
+
+    additional_vim_regex_highlighting = false,
+  },
+}
+
 
 require('username')
---
+++ lua/username/lazy/treesitter.lua
@@ -0,0 +1,4 @@
+return
+{
+  "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = true, build = ":TSUpdate",
+}
